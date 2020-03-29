34a543ef1519587579f75c8987edd24eb03d701d
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-03-29 13:18:45 +0200

    export table`s header to separate function

---
+++ scripts/createTableHeader.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var header = "";
+  header += '<th class="tg-cbli">#</th>';
+  header += '<th class="tg-ul38"></th>';
+  header += '<th class="tg-ul38">Team</th>';
+  header += '<th class="tg-ul38">GP</th>';
+  header += '<th class="tg-ul38">W</th>';
+  header += '<th class="tg-ul38">D</th>';
+  header += '<th class="tg-ul38">L</th>';
+  header += '<th class="tg-ul38">P</th>';
+  header += '<th class="tg-ul38">GF</th>';
+  header += '<th class="tg-ul38">GA</th>';
+  header += '<th class="tg-ul38">GD</th>';
+  $(".header").append(header);
+});
